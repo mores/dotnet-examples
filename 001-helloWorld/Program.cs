847ad45dc1ee59e51f3a3bf3736519bac417b9f8
Stephen More <stephen.more@gmail.com>
2020-12-18 09:03:41 -0500

    Adding simple helloworld to repo.

---
+++ 001-helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotnet-examples
+
+dotnet new gitignore
+
+dotnet new console --output 001-helloWorld
+
+dotnet run --project 001-helloWorld
