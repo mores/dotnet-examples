9be0b9b320381b3186281dab659394e32dcaf375
Stephen More <stephen.more@gmail.com>
2020-12-30 08:50:40 -0500

    Now able to frontend kestrel with apache.

--- 003-webMVC/Startup.cs
+++ 003-webMVC/Startup.cs
@@ -39,7 +39,8 @@ namespace _003_webMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+	    //https now provided by apache
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
